789e400a5fd34702b56c5fa3ef4829c1eef14fd5
SergioAnd95 <developinc@yandex.ru>
2018-11-19 21:42:01 +0200

    Crate project:  - Create basic structure of project and settings  - Create app books with models, admin and fixtures

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from isbn_field import ISBNField
+
+# Create your models here.
+
+
+class Book(models.Model):
+    """
+    Model to represent book in app
+    """
+
+    title = models.CharField(max_length=100)
+    authors_info = models.CharField(max_length=256)
+    isbn = ISBNField(unique=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.title}: {self.price}({self.isbn})'
